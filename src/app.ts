9bee1beb6e3e8a08b25e11f25d9143e6e409abf0
lucas <lucaston2323@gmail.com>
2025-05-28 19:18:05 -0300

    feat: imports de lib e tratamento de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import express from 'express';
 import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+// log
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -11,5 +15,15 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+// tratamento global
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send(error.message);
+});
+
+// teste 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello World");
+})
 
 export default app;
